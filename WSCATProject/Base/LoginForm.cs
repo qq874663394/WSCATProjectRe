32703c66779a9de6cc3118005455894ec24bbad2
Administrator <Administrator@USER-20160704SV>
2016-08-04 18:22:20 +0800

    添加99999

--- WSCATProject/Base/LoginForm.cs
+++ WSCATProject/Base/LoginForm.cs
@@ -20,7 +20,7 @@ namespace WSCATProject.Base
         {
             InitializeComponent();
         }
-        string lin = "sj12222222222";
+        string lin = "999999999999999999999999999999999999999999999999999999999999";
         private void buttonXLogin_Click(object sender, EventArgs e)
         {
             User u = new User();
