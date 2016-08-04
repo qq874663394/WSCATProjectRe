9a311b769c3d318c4d3a02367337a65fcaf55abb
LinMuSi <lms@USER-20160704RC>
2016-08-04 17:53:23 +0800

    加了注释

--- WSCATProject/Program.cs
+++ WSCATProject/Program.cs
@@ -20,6 +20,7 @@ namespace WSCATProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
         }
     }
 }
