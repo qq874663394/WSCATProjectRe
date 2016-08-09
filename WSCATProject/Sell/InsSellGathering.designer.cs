f77b0027fdbbbf22e3c01195d76a22ed6695ac82
xd <Administrator@USER-20160704SV>
2016-08-09 17:04:13 +0800

    加了销售单号

--- WSCATProject/Sell/InsSellGathering.designer.cs
+++ WSCATProject/Sell/InsSellGathering.designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InsSellGathering));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle23 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle24 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
@@ -76,6 +76,8 @@
             this.panelDataView = new System.Windows.Forms.Panel();
             this.dataGridViewFujia = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ltxt_salecode = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.toolStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -220,7 +222,7 @@
             this.ltxt_weishou.Border.Class = "SideNavStrip";
             this.ltxt_weishou.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.ltxt_weishou.DisabledBackColor = System.Drawing.Color.White;
-            this.ltxt_weishou.Location = new System.Drawing.Point(847, 46);
+            this.ltxt_weishou.Location = new System.Drawing.Point(855, 50);
             this.ltxt_weishou.Name = "ltxt_weishou";
             this.ltxt_weishou.PreventEnterBeep = true;
             this.ltxt_weishou.ReadOnly = true;
@@ -241,7 +243,7 @@
             this.ltxt_shoukuan.Border.BorderGradientAngle = 0;
             this.ltxt_shoukuan.Border.Class = "SideNavStrip";
             this.ltxt_shoukuan.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.ltxt_shoukuan.Location = new System.Drawing.Point(325, 52);
+            this.ltxt_shoukuan.Location = new System.Drawing.Point(596, 18);
             this.ltxt_shoukuan.MaxLength = 123;
             this.ltxt_shoukuan.Name = "ltxt_shoukuan";
             this.ltxt_shoukuan.PreventEnterBeep = true;
@@ -267,7 +269,7 @@
             this.ltxt_yingshou.Border.Class = "SideNavStrip";
             this.ltxt_yingshou.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
             this.ltxt_yingshou.ForeColor = System.Drawing.Color.DimGray;
-            this.ltxt_yingshou.Location = new System.Drawing.Point(324, 20);
+            this.ltxt_yingshou.Location = new System.Drawing.Point(324, 51);
             this.ltxt_yingshou.Name = "ltxt_yingshou";
             this.ltxt_yingshou.PreventEnterBeep = true;
             this.ltxt_yingshou.Size = new System.Drawing.Size(143, 16);
@@ -474,6 +476,8 @@
             // 
             // panel6
             // 
+            this.panel6.Controls.Add(this.ltxt_salecode);
+            this.panel6.Controls.Add(this.label2);
             this.panel6.Controls.Add(this.ltxt_shishou);
             this.panel6.Controls.Add(this.label3);
             this.panel6.Controls.Add(this.button1);
@@ -530,7 +534,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(480, 48);
+            this.button1.Location = new System.Drawing.Point(751, 14);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(20, 20);
             this.button1.TabIndex = 35;
@@ -563,7 +567,7 @@
             // labTop5
             // 
             this.labTop5.AutoSize = true;
-            this.labTop5.Location = new System.Drawing.Point(776, 56);
+            this.labTop5.Location = new System.Drawing.Point(784, 56);
             this.labTop5.Name = "labTop5";
             this.labTop5.Size = new System.Drawing.Size(65, 12);
             this.labTop5.TabIndex = 8;
@@ -572,7 +576,7 @@
             // labTop4
             // 
             this.labTop4.AutoSize = true;
-            this.labTop4.Location = new System.Drawing.Point(252, 56);
+            this.labTop4.Location = new System.Drawing.Point(523, 22);
             this.labTop4.Name = "labTop4";
             this.labTop4.Size = new System.Drawing.Size(65, 12);
             this.labTop4.TabIndex = 6;
@@ -591,7 +595,7 @@
             // 
             this.labTop2.AutoSize = true;
             this.labTop2.ForeColor = System.Drawing.Color.Black;
-            this.labTop2.Location = new System.Drawing.Point(251, 22);
+            this.labTop2.Location = new System.Drawing.Point(251, 53);
             this.labTop2.Name = "labTop2";
             this.labTop2.Size = new System.Drawing.Size(65, 12);
             this.labTop2.TabIndex = 2;
@@ -636,24 +640,24 @@
             this.dataGridViewFujia.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewFujia.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewFujia.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            dataGridViewCellStyle23.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle23.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            dataGridViewCellStyle23.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(140)));
-            dataGridViewCellStyle23.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle23.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle23.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle23.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridViewFujia.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle23;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(140)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewFujia.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridViewFujia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewFujia.Cursor = System.Windows.Forms.Cursors.Arrow;
-            dataGridViewCellStyle24.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle24.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle24.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(140)));
-            dataGridViewCellStyle24.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle24.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle24.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle24.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewFujia.DefaultCellStyle = dataGridViewCellStyle24;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(140)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewFujia.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridViewFujia.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridViewFujia.GridColor = System.Drawing.SystemColors.AppWorkspace;
             this.dataGridViewFujia.Location = new System.Drawing.Point(0, 0);
@@ -678,6 +682,36 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(251, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "销售单号：";
+            // 
+            // ltxt_salecode
+            // 
+            this.ltxt_salecode.AcceptsReturn = true;
+            this.ltxt_salecode.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            // 
+            // 
+            // 
+            this.ltxt_salecode.Border.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.ltxt_salecode.Border.BorderBottomColor = System.Drawing.Color.Black;
+            this.ltxt_salecode.Border.BorderBottomWidth = 1;
+            this.ltxt_salecode.Border.BorderGradientAngle = 0;
+            this.ltxt_salecode.Border.Class = "SideNavStrip";
+            this.ltxt_salecode.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.ltxt_salecode.ForeColor = System.Drawing.Color.DimGray;
+            this.ltxt_salecode.Location = new System.Drawing.Point(322, 21);
+            this.ltxt_salecode.Name = "ltxt_salecode";
+            this.ltxt_salecode.PreventEnterBeep = true;
+            this.ltxt_salecode.Size = new System.Drawing.Size(143, 16);
+            this.ltxt_salecode.TabIndex = 39;
             // 
             // InsSellGathering
             // 
@@ -761,5 +795,7 @@
         protected DevComponents.DotNetBar.Controls.TextBoxX ltxt_shishou;
         protected System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        protected DevComponents.DotNetBar.Controls.TextBoxX ltxt_salecode;
+        protected System.Windows.Forms.Label label2;
     }
 }
